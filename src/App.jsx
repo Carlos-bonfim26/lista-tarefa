4a3cf63cd9325c18e884f4332fa5e615a24b0c67
carlos <carlosbonfim722@gmail.com>
2024-05-16 17:54:35 -0300

    version1.0

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import './components/ListaTarefas.jsx'
+import ListaTarefas from './components/ListaTarefas.jsx'
+
+function App() {
+ 
+
+  return (
+   <div className="App">
+<ListaTarefas/>
+    </div>
+  )
+}
+
+export default App
